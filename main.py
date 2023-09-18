e8bb85fdbb52c852277cff71d7bb88be043c50f2
maayanchu <maayanchu@gmail.com>
2023-09-18 12:23:27 +0300

    "movement"

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ def soldier_movement(pygame):
     vel = 1
     x = 0
     y = 0
-    if user_input = [pygame.K_LEFT]:
+    if user_input == [pygame.K_LEFT]:
         x -= vel
-    if user_input =[pygame.K_RIGHT]:
+    if user_input == [pygame.K_RIGHT]:
             x += vel
-    if user_input =[pygame.K_UP]:
+    if user_input == [pygame.K_UP]:
             y -= vel
-    if user_input = [pygame.K_DOWN]:
+    if user_input == [pygame.K_DOWN]:
         y += vel
