790831c28923f15fd44988849da43c296ebe1747
maayanchu <maayanchu@gmail.com>
2023-09-18 11:29:58 +0300

    hi

---
+++ Soldier.py
@@ -0,0 +1,15 @@
+SOLDIER_REG_IMG = "solider.png"
+SOLDIER_NIGHT_IMG = "solider_nigth.png"
+STARTING_POSITION =(0,0)
+
+
+# SOLDIER =
+#
+# if SOLDIER.rect.left < 0:
+#     SOLDIER.rect.left = 0
+# if SOLDIER.rect.right > WINDOW_WIDTH:
+#     SOLDIER.rect.right = WINDOW_WIDTH
+# if SOLDIER.rect.top <= 0:
+#     SOLDIER.rect.top = 0
+# if SOLDIER.rect.bottom >= WINDOW_HEIGHT:
+#     SOLDIER.rect.bottom = WINDOW_HEIGHT
