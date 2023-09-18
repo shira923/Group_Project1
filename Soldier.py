a24e85ef4e62afb3e32fba96980a6799fd887c31
maayanchu <maayanchu@gmail.com>
2023-09-18 15:08:17 +0300

    new changes to main and soldier

--- Soldier.py
+++ Soldier.py
@@ -5,11 +5,4 @@ STARTING_POSITION = (0, 0)
 
 
 
-if SOLDIER.rect.left < 0:
-    SOLDIER.rect.left = 0
-if SOLDIER.rect.right > WINDOW_WIDTH:
-    SOLDIER.rect.right = WINDOW_WIDTH
-if SOLDIER.rect.top <= 0:
-    SOLDIER.rect.top = 0
-if SOLDIER.rect.bottom >= WINDOW_HEIGHT:
-    SOLDIER.rect.bottom = WINDOW_HEIGHT
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import Soldier
+import Consts
 def soldier_movement(pygame):
     user_input = pygame.key.get_pressed()
     vel = 1
@@ -20,6 +22,19 @@ class player(pygame.sprite):
        self_images = []
        img = pygame.image.load(os.path.join('Group_Project1', Consts.SOLDIER_REG_IMAGE)).convert()
        self.images.append(img)
-       self.image = self.images[Consts.STARTING_POSITION]
+       self.image = self_images[Consts.STARTING_POSITION]
        self.rect = self.image.get_rect()
+
+
+def soldier_out_of_borders(Main, Consts):
+    player = Main.initiate_player()
+    if player.rect.left < 0:
+        player.rect.left = 0
+    if player.rect.right > Consts.FIELD_ROWS:
+        player.rect.right = Consts.FIELD_COLS
+    if player.rect.top <= 0:
+        player.rect.top = 0
+    if player.rect.bottom >= Consts.FIELD_ROWS:
+        player.rect.bottom = Consts.FIELD_ROWS
+
 
