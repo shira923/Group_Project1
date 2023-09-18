a721e967c06d6326eb0a65ec6852f5c1e0e3bca4
IditAmor <idit.amor111@gmail.com>
2023-09-18 11:41:58 +0300

    creation of game field matrix

--- GameField.py
+++ GameField.py
@@ -3,6 +3,7 @@ FIELD_ROWS = 25
 FIELD_COLS = 50
 EMPTY_SQUARE = "EMPTY"
 
+
 def create_game_field():
     global game_field
     for row in range(FIELD_ROWS):
@@ -10,5 +11,4 @@ def create_game_field():
         for col in range(FIELD_COLS):
             row.append(EMPTY_SQUARE)
         game_field.append(row)
-
 
