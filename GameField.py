77e7b7d3f4ac4864f6bb2a216e233b5aa24abf8b
shira923 <shirava246@gmail.com>
2023-09-19 09:46:29 +0300

    hiiii

--- GameField.py
+++ GameField.py
@@ -6,35 +6,34 @@ import Consts
 game_field = []
 mines_list = []
 
-def create(line, column, center_x, center_y):
+def create(line, column):
     return {"number": None,
-             "mine": None,
-            "center_x" : center_x,
-            "center_y" : center_y}
+            "center_x" : column * 10,
+            "center_y" : line * 10}
 
-def recalc_bubbles_locations():
-    # There is no need to do so for the first line, since it was just added
-    for row in range(1, len(game_field)):
-        for col in range(50):
-            game_field[row][col]["center_x"] = Bubble.calc_center_x(col, row,
-                                                                      row_start=0)
-            game_field[row][col]["center_y"] = Bubble.calc_center_y(row)
-def create_game_field():
-    global game_field
-    for row in range(Consts.FIELD_ROWS):
-        row = []
-        for col in range(Consts.FIELD_COLS):
-            row.append(Consts.EMPTY_SQUARE)
-        game_field.append(row)
+# def recalc_bubbles_locations():
+#     # There is no need to do so for the first line, since it was just added
+#     for row in range(1, len(game_field)):
+#         for col in range(50):
+#             game_field[row][col]["center_x"] = Bubble.calc_center_x(col, row,
+#                                                                       row_start=0)
+#             game_field[row][col]["center_y"] = Bubble.calc_center_y(row)
+# def create_game_field():
+#     global game_field
+#     for row in range(Consts.FIELD_ROWS):
+#         row = []
+#         for col in range(Consts.FIELD_COLS):
+#             row.append(Consts.EMPTY_SQUARE)
+#         game_field.append(row)
 
 
-def calc_center(col, row):
-    x = row * 10
-    y = col * 10
-    return (x , y)
+# def calc_center(col, row):
+#     x = row * 10
+#     y = col * 10
+#     return (x , y)
 
-def calc_x():
-    x = col * 10
+# def calc_x():
+#     x = col * 10
 
 # def calc_center_x(col, row,):
 #     x = row * 10
@@ -58,7 +57,7 @@ def put_grass_in_grid():
     check = (0, 0)
     for num in range(20):
         line = random.randint(0, 21)
-        column = random.randint(0, 50)
+        column = random.randint(0, 49)
         while line == 0 and column == 0 or line == 25 and column == 50:
             line = random.randint(0, 21)
             column = random.randint(0, 50)
@@ -73,13 +72,13 @@ def put_grass_in_grid():
 def put_mines_in_grid():
     for num in range(20):
         line = random.randint(0, 21)
-        column = random.randint(0, 50)
-        while line == 0 and column == 0 or line == 25 and column == 50:
-            line = random.randint(0, 25)
-            column = random.randint(0, 50)
+        column = random.randint(0, 49)
+        while line == 0 and column == 0 or game_field[line][column]["number"] != Consts.FLAG_NUM:
+            line = random.randint(0, 21)
+            column = random.randint(0, 49)
         if game_field[line][column]["number"] == Consts.GRASS_NUM:
             game_field[line][num]["number"] = Consts.MINE_NUM
-            pygame.surface.blit(Consts.MINE_IMG, Consts.GRASS_IMG, [line * 10, column * 10])
+            pygame.surface.blit(Consts.MINE_IMG, Consts.GRASS_IMG, create(line, column))
         mines_list.append([line, column])
         for l in range(column, column + 3):
             game_field[line][column]["number"] = Consts.MINE_NUM
