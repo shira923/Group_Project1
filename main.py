d491bc0da980311f37b6ee02a95dff650d6a9542
maayanchu <maayanchu@gmail.com>
2023-09-19 10:02:15 +0300

    new changes to main and soldier

--- main.py
+++ main.py
@@ -9,16 +9,5 @@ Soldier.soldier_movement(pygame)
 
 def handle_player_events():
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            state["is_window_open"] = False
-        elif state["state"] != Consts.RUNNING_STATE:
-            continue
-
-        if event.type == pygame.MOUSEMOTION:
-            rotate_arrow()
-
-        elif event.type == pygame.MOUSEBUTTONDOWN and \
-                not state["is_bubble_fired"] and \
-                not state["bubbles_popping"]:
-            fire_bubble()
+        if event.type == pygame.pygame.KEYDOWN:
 
