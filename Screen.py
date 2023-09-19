58e0950ef98bfa99aeb4ec657dd576143ea1dbb0
IditAmor <idit.amor111@gmail.com>
2023-09-19 09:57:44 +0300

    screen

--- Screen.py
+++ Screen.py
@@ -22,3 +22,29 @@ def put_grass_in_grid():
         pygame.transform(Consts.GRASS_IMG, (line, column))
         grass_list.append((line, column))
     return grass_list
+
+
+font = pygame.font.SysFont('timesnewroman', 60)
+
+
+def win_msg():
+    win_txt = font.render("YOU WON!", False, (255, 255, 255))
+    screen.blit(win_txt, (12.5, 225))
+    pygame.display.update()
+
+
+def lose_msg():
+    win_txt = font.render("YOU LOST!", False, (255, 255, 255))
+    screen.blit(win_txt, (12.5, 225))
+    pygame.display.update()
+
+
+def draw_game():
+    screen.fill(Consts.green_color)
+    GameField.create_game_field()
+    if GameField.should_soldier_pop():
+        lose_msg()
+    elif GameField.touching_flag():
+        win_msg()
+
+    pygame.display.flip()
